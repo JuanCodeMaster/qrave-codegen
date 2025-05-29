53b28edab0cabdb0598f23f675114dce4500280b
JuanCodeMaster <juan.velez-al@uniminuto.edu.co>
2025-05-28 21:28:58 -0500

    Nuevo servicio de QR

--- src/main/java/com/qrave/qrservice/config/SecurityConfig.java
+++ src/main/java/com/qrave/qrservice/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8100")); // 👈 Cambia por tu frontend si aplica
+        config.setAllowedOrigins(List.of("http://localhost:8100","https://localhost")); // 👈 Cambia por tu frontend si aplica
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
